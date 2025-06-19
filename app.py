2f7cf86d7472a85822887a5de136ca18229da573
suresh <anthatisureshgoud55@gmail.com>
2025-06-19 15:35:37 +0530

    Add port to app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from routes import configure_routes
 
-app = Flask(__name__, static_folder='static', template_folder='templates')
+app = Flask(__name__)
 
 # Enable CORS for all routes
 CORS(app)
@@ -11,4 +11,5 @@ CORS(app)
 configure_routes(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
