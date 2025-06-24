36ba99daade004052c249f63ca3e99bf1ee5d407
Suresh_Web_Dev <129653772+sureshgit001@users.noreply.github.com>
2025-06-24 14:40:01 +0530

    Update detection.py

--- detection.py
+++ detection.py
@@ -6,7 +6,7 @@ import mediapipe as mp
 
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(
-    static_image_mode=False,
+    static_image_mode=True,
     max_num_faces=1,
     refine_landmarks=True,
     min_detection_confidence=0.5,
